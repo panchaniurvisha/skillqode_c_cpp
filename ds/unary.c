02b67a211aa3e28d1931bcb4d90203667d5a3572
urvisha <urvisha1109@gmail.com>
2022-11-17 10:21:47 +0530

    first commit

---
+++ ds/unary.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+    int a=10;
+    //printf("%d %d %d",++a,a,a++);
+    printf("%d %d %d",a,a++,a);
+    //printf("%d %d %d",++a,a,++a);
+     //printf("%d %d %d",++a,a,a++);
+      //printf("%d %d %d",--a,a,--a);
+    return 0;
+}
