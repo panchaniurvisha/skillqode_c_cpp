d5866a61121211a4e53169fa0fab3e81a58f4863
urvisha <urvisha1109@gmail.com>
2023-01-02 10:38:28 +0530

    first commit

---
+++ dart/null_safety.dart
@@ -0,0 +1,10 @@
+import 'dart:io';
+
+void main() {
+  int? x, y, sum = 0;
+  print("enter value:");
+  x = int.parse(stdin.readLineSync()!);
+  y = int.parse(stdin.readLineSync()!);
+  sum = y + x;
+  print("sum=$sum");
+}
